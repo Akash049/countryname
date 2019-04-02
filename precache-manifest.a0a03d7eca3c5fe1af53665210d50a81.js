fafbef5c0eeab2a026de1a1e962c3b3ab080d4f1
Akash049 <akashchandra733@gmail.com>
2019-04-02 14:27:45 +0530

    Updates

---
+++ precache-manifest.a0a03d7eca3c5fe1af53665210d50a81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0adcddf4ed1b914375d7",
+    "url": "/countryname/static/js/runtime~main.677cf154.js"
+  },
+  {
+    "revision": "91b08248799905253809",
+    "url": "/countryname/static/js/main.8b01472f.chunk.js"
+  },
+  {
+    "revision": "d1f434c69ac3566437d8",
+    "url": "/countryname/static/js/2.c437f428.chunk.js"
+  },
+  {
+    "revision": "91b08248799905253809",
+    "url": "/countryname/static/css/main.0d27477a.chunk.css"
+  },
+  {
+    "revision": "89dc7731d232a671aa6225009986ea5e",
+    "url": "/countryname/index.html"
+  }
+];
